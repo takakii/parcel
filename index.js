c5d126b55b98d4c73d8275b0b93752b134418a4b
Takaki Yasuma <takakii@hatena.ne.jp>
2017-12-16 22:22:36 +0900

    更新ファイルを追加

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "babel-polyfill";
-// import "./app";
-
-import './scss/test.scss';
+import "./scss/test.scss";
 
 class HelloMessage extends React.Component {
   render() {
